7d4aa9d9710a8d6c11965c3b0bc3aa3793f93c25
MushvigShukurov <musfiqsukurov9@gmail.com>
2023-01-13 21:21:02 +0400

    Yeni paketler ve urls

--- Core/settings.py
+++ Core/settings.py
@@ -38,7 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_extensions',
+    'dj_rest_auth',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',# sosiali istesek
+    'dj_rest_auth.registration',
+    'django.contrib.sites',
     # 'profiller',
     'profiller.apps.ProfillerConfig',
 ]
@@ -132,3 +139,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATIONS = 'none'
+ACCOUNT_EMAIL_REQUIRED = (True,)
--- Core/urls.py
+++ Core/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/',include('rest_framework.urls')),
+    path('api/rest-auth/',include('dj_rest_auth.urls')),
+    path('api/rest-auth/registration/',include('dj_rest_auth.registration.urls')),
+    path('api/',include("profiller.api.urls")),
 ]
-
+from django.conf import settings
+from django.conf.urls.static import static
 if settings.DEBUG == True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
