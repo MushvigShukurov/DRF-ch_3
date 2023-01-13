7f7f3fb5707c5d4c0d78fff0547b6e7d933a85ba
MushvigShukurov <musfiqsukurov9@gmail.com>
2023-01-14 02:32:53 +0400

    +Viewsets & Router

--- profiller/api/urls.py
+++ profiller/api/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
-from profiller.api import views as api_views
+from django.urls import path, include
+# from profiller.api import views as api_views
+from profiller.api.views import ProfilViewSet
+from rest_framework.routers import DefaultRouter
+# profil_list = ProfilViewSet.as_view({'get':'list'})
+# profil_detay = ProfilViewSet.as_view({'get':'retrieve'})
+
+router = DefaultRouter()
+router.register(r'kullanici-profilleri',ProfilViewSet)
+
 
 urlpatterns = [
-    path('kullanici-profilleri/',api_views.ProfilList.as_view(),name="profiller"),
+    # path('kullanici-profilleri/',api_views.ProfilList.as_view(),name="profiller"),
+    # path('kullanici-profilleri/', profil_list, name="profiller"),
+    # path('kullanici-profilleri/<int:pk>', profil_detay, name="profiller"),
+    path('', include(router.urls)),
     
 ]
+
--- profiller/api/views.py
+++ profiller/api/views.py
@@ -2,9 +2,31 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from profiller.models import Profil
 from profiller.api.serializers import ProfilSerializer
+# from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+from profiller.api.permissions import OzProfiliYadaReadOnlyBrat
+# class ProfilList(generics.ListAPIView):
+#     queryset = Profil.objects.all()
+#     serializer_class  = ProfilSerializer
+#     permission_classes = [IsAuthenticated]
 
 
-class ProfilList(generics.ListAPIView):
+
+# class ProfilViewSet(ReadOnlyModelViewSet):
+#     queryset = Profil.objects.all()
+#     serializer_class  = ProfilSerializer
+#     permission_classes = [IsAuthenticated]
+
+
+class ProfilViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    # mixins.DestroyModelMixin,
+    GenericViewSet
+):
     queryset = Profil.objects.all()
     serializer_class  = ProfilSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [OzProfiliYadaReadOnlyBrat]
